d8f15b3d768da73fc980a008799418681b37aecb
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-09-16 12:04:16 -0700

    Remove index

--- packages/server/index.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-
-mongoose
-  .connect('mongodb+srv://abdulhathi_db_user:fF8ESWCeQHRmjD1D@portfolio-cluster.mart1ws.mongodb.net/')
-  .then(() => console.log('DB Connected'))
-  .catch((err) => console.log(err))
-
